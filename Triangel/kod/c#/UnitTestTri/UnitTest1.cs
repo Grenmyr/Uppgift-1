452ed79cf6abb249b1eb007cbc4f2507fe7cd840
Grenmyr <dg222cs@student.lnu.se>
2013-11-13 15:39:36 +0100

    Whitebox Testing Setup

---
+++ Triangel/kod/c#/UnitTestTri/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTri
+{
+    [TestClass]
+    public class TriangleUnitTest
+    {
+        [TestMethod]
+        public void isIsoscelesTest()
+        {
+            Triangle tri = new Triangle(1, -3.5, 5);
+            Assert.IsTrue(tri.isIsosceles());
+        }
+    }
+}
