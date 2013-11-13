636fc75b0caa08c91109accd1e951e40c3a30c45
Grenmyr <dg222cs@student.lnu.se>
2013-11-13 16:10:36 +0100

    Klar med Testning avIslosceles Metoded - Whitebox

--- Triangel/kod/c#/UnitTestTri/UnitTest1.cs
+++ Triangel/kod/c#/UnitTestTri/UnitTest1.cs
@@ -7,10 +7,20 @@ namespace UnitTestTri
     public class TriangleUnitTest
     {
         [TestMethod]
+        // Test av metod för Likbent triangel
         public void isIsoscelesTest()
         {
-            Triangle tri = new Triangle(1, -3.5, 5);
-            Assert.IsTrue(tri.isIsosceles());
+            // Test av Oliksidig Triangel
+            Triangle tri1 = new Triangle(1, 3.5 , 5);
+            Assert.IsFalse(tri1.isIsosceles());
+            
+            // Test av Liksidig triangel
+            Triangle tri2 = new Triangle(3.0, 3.0, 3.0);
+            Assert.IsFalse(tri2.isIsosceles());
+
+            // Test av Likbent triangel
+            Triangle tri3 = new Triangle(3, 1, 3.0);
+            Assert.IsTrue(tri3.isIsosceles());
         }
     }
 }
