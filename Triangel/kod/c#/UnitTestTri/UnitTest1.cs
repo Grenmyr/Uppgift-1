f71adfc34222ef03e4e337327213b1e5e5140a76
Grenmyr <dg222cs@student.lnu.se>
2013-11-14 11:51:46 +0100

    Jobbar vidare och börjat testa double [] konstruktorn

--- Triangel/kod/c#/UnitTestTri/UnitTest1.cs
+++ Triangel/kod/c#/UnitTestTri/UnitTest1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestTri
@@ -31,7 +34,7 @@ namespace UnitTestTri
             Assert.IsFalse(tri1.isIsosceles());
 
             // Test av Liksidig triangel
-            Triangle tri2 = new Triangle(4.0, 4, 4.0);
+            Triangle tri2 = new Triangle(1011.0, 1011, 1011.0);
             Assert.IsTrue(tri2.isIsosceles());
 
             // Test av Likbent triangel
@@ -49,11 +52,38 @@ namespace UnitTestTri
             // Test av Liksidig triangel
             Triangle tri2 = new Triangle(101, 101.00, 101);
             Assert.IsFalse(tri2.isIsosceles());
-
+            Console.WriteLine("bosse banan");
             // Test av Likbent triangel
             Triangle tri3 = new Triangle(77, 77, 1.0);
             Assert.IsFalse(tri3.isIsosceles());
         }
-      
+        [TestMethod]
+        // Test av double för konstruktor
+         public void Triangle()
+        {
+            // tilldelar min "fältet sides" som är en array av typ double värden 1, fältet hämtas med hjälp av mats getfieldvalue metod som kopierats skamlöst.
+            double[] sides  =  (double[])GetFieldValue(new Triangle(1,1,1), "sides");
+
+            // Testar att reclection fungerar och att konstruktorn kan tilldela fältet sides värde.
+            Assert.IsTrue(sides[0] == 1 && sides[1] == 1 && sides[2] == 1);
+
+            // Testar att konstrutorn inte godkänner 0. Använder 0,1,1 eftersom jag vet att 1 tidigare fungerade. ******FAIL******
+            double[] sides2 = (double[])GetFieldValue(new Triangle(0, 1, 1), "sides");
+            CollectionAssert.AreNotEqual(new double [] {0,1,1}, sides2);
+        }
+         
+        
+        // Kopierat från Mats Lock!
+        private static object GetFieldValue(object o, string name)
+        {
+
+            var field = o.GetType().GetField(name, BindingFlags.NonPublic | BindingFlags.Instance);
+            if (field == null)
+            {
+                throw new ApplicationException(String.Format("FEL! Det privata fältet {0} saknas.", name));
+            }
+            return field.GetValue(o);
+        }
+        
     }
 }
